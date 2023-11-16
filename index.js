e4d912345f033875a29be18773e61b2ae8764a2b
luislourencos <luis.lourencos@gmail.com>
2023-11-16 23:30:13 +0100

    feat: add transitions

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const app = document.getElementById("app");
 const loading =  document.getElementById("loading");
 
-loading.style.color = 'white';
+loading.style.color = 'violet';
 
-async function getPhotos(number=12) {
-    loading.textContent = 'Loading...'
+async function getPhotos(number=12) { // number by default of photos
+    loading.textContent = 'Loading...' // set text Loading
     app.textContent=''
     try {
-        const randomNumber = Math.floor(Math.random() * (200) + 1)
+        // this is to generate a diferents photos
+        const randomNumber = Math.floor(Math.random() * 200)
 
         const resp = await fetch('https://jsonplaceholder.typicode.com/photos')
         const data = await resp.json()
         const dataSlice = data.slice(randomNumber, randomNumber+number)
 
         setTimeout(() => {
-            printList(dataSlice)
-            
+            printList(dataSlice) 
         }, 1000);
 
     } catch (error){
         console.error(error)
     } finally {
         setTimeout(() => {
-            loading.textContent = ''  
+            loading.textContent = ''  // remove Loading aften time
         }, 900);
     }
 }
 
-
+// this funtion crint de card list
 function printList(data=[]) {
     let list=''
     data.map((element)=>{
@@ -36,11 +36,12 @@ function printList(data=[]) {
     app.innerHTML = `<ul>${list}</ul>`
 }
 
+// a simple card with images
 function cart(id, title){
     return (
         `<li class='card'>
             <img src="https://picsum.photos/id/${id}/200/300" alt="${title}" >
-            <p>${title}</>
+            <div class='title'>${title}</div>
         </li>` 
     )
 }  
